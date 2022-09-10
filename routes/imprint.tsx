d784b0949ce524fde597776a67335c46be7f98bd
Florian <76884340+FlosWelt@users.noreply.github.com>
2022-09-10 12:33:49 +0200

    Add files via upload

---
+++ routes/imprint.tsx
@@ -0,0 +1,39 @@
+
+export default function Home() {
+    return (
+      <div  class=" w-full h-screen  bg-repeat bg-gradient-to-l from-purple-400 via-purple-300 to-indigo-300">
+          <head>
+            <title>Imprint</title>
+            <meta name="title" content="Imprint"/>
+            <meta name="description" content="My Imprint"/>
+  
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://floswelt.com/"/>
+            <meta property="og:title" content="Imprint"/>
+            <meta property="og:description" content="My Imprint"/>
+  
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://floswelt.com/"/>
+            <meta property="twitter:title" content="Imprint"/>
+            <meta property="twitter:description" content="My Imprint"/>
+    </head>
+        <div class="flex flex-col items-center justify-center inline-block">
+        <h1 class="text-4xl text-center py-72 font-semibold inline-block">Imprint <img alt="scroll" class="inline-block  h-12 w-12 " src="./scroll.png"></img></h1>
+        <p class="font-medium -my-72 px-2">Information according to § 5 Telemediengesetz (TMG)</p>
+        <h2 class="text-2xl text-center py-72 my-3 font-semibold inline-block">Responsible person acc. § 55 II RStV:</h2>
+        <p class="font-medium -my-72 px-2">Florian Schwagereit</p>
+        <h3 class="text-xl font-semibold  text-center py-72 my-3">Contact</h3>
+        <p class="font-medium -my-72 px-2 ">Florian Schwagereit  </p>
+        <p class="font-medium my-72 px-2 "> Detailed address on request,</p>
+        <p class="font-medium -my-72 px-2 ">  Krefeld</p>
+        <p class="font-medium my-72 px-2 "> Germany</p>
+        <p class="font-semibold -my-[16rem] px-2 ">This imprint applies to the following websites:</p>
+        <a href="https://floswelt.com" class="font-medium my-72 px-2 " target="_blank">floswelt.com</a>
+        <a href="https://ip-address.app" class="font-medium -my-72 px-2 " target="_blank">ip-address.app</a>
+
+    </div>
+  
+      </div>
+    );
+  }
+  
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,26 +4,26 @@ export default function Home() {
     <div  class=" w-full h-screen  bg-repeat bg-gradient-to-l from-purple-400 via-purple-300 to-indigo-300">
         <head>
           <title>Hello World - Florian S.</title>
-          <meta name="title" content="Hellow World - Florian S."/>
+          <meta name="title" content="Hello World - Florian S."/>
           <meta name="description" content="My simple personal website"/>
 
           <meta property="og:type" content="website"/>
           <meta property="og:url" content="https://floswelt.com/"/>
-          <meta property="og:title" content="Hellow World - Florian S."/>
+          <meta property="og:title" content="Hello World - Florian S."/>
           <meta property="og:description" content="My simple personal website"/>
 
           <meta property="twitter:card" content="summary_large_image"/>
           <meta property="twitter:url" content="https://floswelt.com/"/>
-          <meta property="twitter:title" content="Hellow World - Florian S."/>
+          <meta property="twitter:title" content="Hello World - Florian S."/>
           <meta property="twitter:description" content="My simple personal website"/>
   </head>
       <div class="flex flex-col items-center justify-center inline-block">
-      <h1 class="text-4xl text-center py-72 font-semibold inline-block">Hello World <img alt="waving hand" class="inline-block  h-12 w-12 " src="./wave.png"></img></h1>
-      <p class="font-medium -my-72 px-2">I am Florian and i make stuff, below is some stuff i made</p>
+      <h1 class="text-4xl text-center sm:py-72 py-56 font-semibold inline-block">Hello World <img alt="waving hand" class="inline-block  h-12 w-12 " src="./wave.png"></img></h1>
+      <p class="font-medium sm:-my-72 -my-56 px-2">I am Florian and i make stuff, below is some stuff i made</p>
       </div>
       <div class="flex flex-col items-center justify-center inline-block">
-      <h2 class="font-semibold text-2xl text-center my-[20.5rem]">Projects</h2>
-        <div class="relative  -my-80">
+      <h2 class="font-semibold text-2xl text-center sm:my-[20.5rem] my-64">Projects</h2>
+        <div class="relative  sm:-my-80 -my-64">
            <div class="z-50 p-3 mt-10 max-w-xs  grid grid-cols-1 cursor-default select-none rounded bg-gray-400 dark:bg-gray-900 shadow-lg">
               <div class="font-semibold text-xl mb-2 ml-2 text-gray-900 dark:text-white">ip-address.app</div>
              <div class="flex">
